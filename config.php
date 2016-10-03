5eedb6452137d2c114e7d8786cf15c89219026f7
Matt182 <mattspring0@gmail.com>
2016-10-04 00:41:32 +0300

    initial

--- config.php
+++ config.php
@@ -4,10 +4,10 @@ error_reporting(-1);
 ini_set("display_errors", 1);
 
 //delete line below
-//throw new Exception("Plese edit configs in config.php", 1);
+throw new Exception("Plese edit configs in config.php", 1);
 
-putenv ('driver=mysql');
-putenv ('dbname=viliot');
-putenv ('host=localhost');
-putenv ('username=root');
-putenv ('password=root');
+putenv ('driver=');
+putenv ('dbname=');
+putenv ('host=');
+putenv ('username=');
+putenv ('password=');
